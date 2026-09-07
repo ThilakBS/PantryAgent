b311aec34eec0bd175d1c785084099067c333f39
Thilak Balaji Sharmila <thilakbs202x@gmail.com>
2026-09-06 22:07:26 -0400

    fix tables to remove bugs and have a logical order

--- backend/init.sql
+++ backend/init.sql
@@ -1,18 +1,3 @@
--- creates virtual pantry to store food info
-CREATE TYPE cate AS ENUM ('meat', 'veg', 'fruit', 'frozen', 'drinks', 'snacks', 'dry', 'mis');
-CREATE TYPE u AS ENUM ('mL', 'L', 'g', 'kg', 'oz', 'lb', 'count');
-
-CREATE TABLE pantry (
-    obj_id      SERIAL PRIMARY KEY,
-    user_id     INTEGER NOT NULL,
-    food_name   TEXT NOT NULL,
-    quantity    FLOAT NOT NULL CHECK (quantity >= 0),
-    unit        u NOT NULL,
-    date_added  DATE NOT NULL DEFAULT CURRENT_DATE,
-    expiration  DATE,
-    category    cate NOT NULL
-);
-
 -- stores info about users
 CREATE TABLE users (
     user_id     SERIAL PRIMARY KEY,
@@ -21,12 +6,26 @@ CREATE TABLE users (
     join_date   DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
--- stores convos by session
+-- creates virtual pantry to store food info
+CREATE TYPE cate AS ENUM ('meat', 'veg', 'fruit', 'frozen', 'drinks', 'snacks', 'dry', 'mis');
+CREATE TYPE u AS ENUM ('mL', 'L', 'g', 'kg', 'oz', 'lb', 'count');
 
-CREATE TABLE convos (
-    convo_id    SERIAL PRIMARY KEY
+CREATE TABLE pantry (
+    obj_id      SERIAL PRIMARY KEY,
     user_id     INTEGER NOT NULL REFERENCES users(user_id),
-    ses_time    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_DATE,
+    food_name   TEXT NOT NULL,
+    quantity    FLOAT NOT NULL CHECK (quantity >= 0),
+    unit        u NOT NULL,
+    date_added  DATE NOT NULL DEFAULT CURRENT_DATE,
+    expiration  DATE,
+    category    cate NOT NULL
+);
+
+-- stores convos by session
+CREATE TABLE convos (
+    convo_id    SERIAL PRIMARY KEY,
+    user_id     INTEGER NOT NULL REFERENCES users(user_id),
+    ses_time    TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title       text
 );
 
@@ -34,10 +33,9 @@ CREATE TABLE convos (
 CREATE TYPE roles AS ENUM ('user', 'assistant');
 
 CREATE TABLE messages (
-    mes_id      SERIAL PRIMARY KEY
-    user_id     INTEGER NOT NULL REFERENCES users(user_id),
-    convo_id    INTEGer NOT NULL REFERENCES users(convo_id),
+    mes_id      SERIAL PRIMARY KEY,
+    convo_id    INTEGER NOT NULL REFERENCES convos(convo_id),
     mes_role    roles NOT NULL,
-    content     text,
-    convo_time  DATE NOT NULL DEFAULT CURRENT_DATE
+    content     text NOT NULL,
+    convo_time  TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
