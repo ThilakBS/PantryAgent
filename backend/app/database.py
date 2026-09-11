c3a67c576f9f4ac0080748a4613393ab103bf852
Thilak Balaji Sharmila <thilakbs202x@gmail.com>
2026-09-10 23:25:49 -0400

    esyablish base connection to Supabase for FastAPI

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,18 @@
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv #Import .env values
 
 load_dotenv() #load .env values
 
 d_base = os.environ["DATABASE_URL"]
+
+engine = create_engine(d_base)
+session = sessionmaker(bind=engine)
+
+def get_db():
+    db = session()
+    try:
+        yield db
+    finally:
+        db.close()
