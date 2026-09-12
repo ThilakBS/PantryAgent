3002cfb7677d7c40083aae3e45f3266a7aeb6356
Thilak Balaji Sharmila <thilakbs202x@gmail.com>
2026-09-11 23:40:28 -0400

    correct bugs in user model

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,15 +1,16 @@
-from sqlalchemy.orm import declarative_base, Mapped,mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped,mapped_column
 from sqlalchemy import Text, Column, Date, Integer, Float, TIMESTAMP, DateTime, Enum, ForeignKey
+import datetime
 
-class Base(declarative_base):
+class Base(DeclarativeBase):
     pass
 
 # Model of users SQL Table
-class User():
+class User(Base):
     __tablename__ = 'users'
 
     user_id: Mapped[int] = mapped_column(primary_key=True)
     user_name: Mapped[str]
     contact: Mapped[str]
-    join_date: Mapped[Date]
+    join_date: Mapped[datetime.date]
 
